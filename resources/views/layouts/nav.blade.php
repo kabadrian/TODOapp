0f39b12df8517f78172aa8030aeca0456f2d387a
kabadrian <kabadrian2@gmail.com>
2022-02-12 09:54:56 +0100

    Add top navigation

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container-md">
+        <a class="navbar-brand" href="{{route('todos.index')}}}">Todo app</a>
+
+        <!-- If user is logged in display logout button, otherwise display login button-->
+        @auth
+            <form action="{{route('logout')}}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-light">Odhlásiť</button>
+            </form>
+        @else
+            <a href="{{route('login')}}" class="btn btn-light">Prihlásiť</a>
+        @endauth
+    </div>
+
+</nav>
