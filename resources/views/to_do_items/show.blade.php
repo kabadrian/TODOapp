5c5baa86cd254e6ffc993a885238fb9ba6afb5a5
kabadrian <kabadrian2@gmail.com>
2022-02-12 20:39:19 +0100

    Add author to todo details

--- resources/views/to_do_items/show.blade.php
+++ resources/views/to_do_items/show.blade.php
@@ -5,6 +5,9 @@
     <h1 class="mt-3">Detail úlohy: {{$todo->title}}</h1>
 
     <div class="my-3 p-4 bg-secondary text-white rounded">
+
+        <div class="h4">Autor úlohy: {{$todo->user->name}}</div>
+
         <div class="h3">Názov úlohy</div>
         <hr>
         <p>{{$todo->title}}</p>
@@ -12,6 +15,7 @@
         <div class="h3">Detail úlohy</div>
         <hr>
         <p>{{$todo->description}}</p>
+
     </div>
 
 @endsection
